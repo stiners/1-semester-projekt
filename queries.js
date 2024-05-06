a175b0fb722e022ad6540d03447527e9d739a4e7
stiners <stine-rs@hotmail.com>
2024-05-06 08:57:56 +0200

    added database

---
+++ queries.js
@@ -0,0 +1,123 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+const csvtojson = require("csvtojson");
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+//route for /foods
+const getFoods = (request, response) => {
+  pool.query("SELECT * FROM food_tmp", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+//route for /insert-food
+const insertFood = (request, response) => {
+  const {
+    produkt,
+    kategori,
+    co2e_pr_kg,
+    landbrug,
+    forarbejdning,
+    emballage,
+    transport,
+    detail,
+  } = request.body;
+  pool.query(
+    `INSERT INTO food_tmp (produkt, kategori, co2e_pr_kg, landbrug, forarbejdning, emballage, transport, detail) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`,
+    [
+      produkt,
+      kategori,
+      co2e_pr_kg,
+      landbrug,
+      forarbejdning,
+      emballage,
+      transport,
+      detail,
+    ],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(201).send(`Food added`);
+    }
+  );
+};
+
+//route for /populateFoods
+const populateFoods = (request, response) => {
+  const fooddata = "food_data.csv";
+  const options = {
+    delimiter: ";",
+  };
+
+  csvtojson()
+    .fromFile(fooddata, options)
+    .then((source) => {
+      //Fetching the data from each row
+      //and inserting to the table food_tmp
+      for (var i = 0; i < source.length; i++) {
+        var produkt = source[i]["Produkt"];
+        var kategori = source[i]["Kategori"];
+        var co2e_pr_kg = source[i]["Total kg CO2e/kg"];
+        var landbrug = source[i]["Landbrug"];
+        var forarbejdning = source[i]["Forarbejdning"];
+        var emballage = source[i]["Emballage"];
+        var transport = source[i]["Transport"];
+        var detail = source[i]["Detail"];
+
+        console.log("source: ", source[0]);
+
+        console.log("produkt: ", produkt);
+        console.log("kategori: ", kategori);
+
+        //TODO: fortsæt med de andre kolonner
+
+        var insertStatement =
+          "INSERT INTO food_tmp(produkt, kategori, co2e_pr_kg, landbrug, forarbejdning, emballage, transport, detail) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)";
+        var items = [
+          produkt,
+          kategori,
+          co2e_pr_kg,
+          landbrug,
+          forarbejdning,
+          emballage,
+          transport,
+          detail,
+        ];
+
+        //TODO: her skal laves to variabler: insertStatement og items.
+        //insertStatement skal bestå af sådan som du vil indsætte data i food_tmp tabellen, men med
+        //placeholders $1, $2 osv i stedet for værdier
+        //items er en array med de variabler der er blevet defineret ud fra vores data lige ovenover
+
+        //Inserting data of current row into database
+        pool.query(insertStatement, items, (err, results, fields) => {
+          if (err) {
+            console.log("Unable to insert item at row " + i + 1);
+            return console.log(err);
+          }
+        });
+      }
+      response.status(201).send("All foods added");
+    });
+};
+
+module.exports = {
+  getFoods,
+  insertFood,
+  populateFoods,
+};
+
+// In the context of parameterized queries using the pg library in Node.js, the placeholders are represented by $1, $2, and so on, instead of using ${name} syntax
+// The reason for this difference is that the $1, $2 syntax is specific to the pg library and the PostgreSQL query protocol. It is used to bind parameters securely and efficiently in the query.
+// When using parameterized queries with the pg library, you pass the actual values as an array in the second parameter of the query() function. The library internally maps these values to the corresponding placeholders in the SQL query string based on their position in the array.
+// Therefore, in the given code snippet, you should continue using $1, $2, and $3 placeholders to represent the variables name, email, and id, respectively, instead of using the ${name} syntax.
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+// const cors = require("cors");
+const db = require("./queries");
+const port = process.env.PORT || 4000;
+// const foodItem = document.getElementById("foodItem");
+// const foodCategory = document.getElementById("foodCategory");
+
+require("dotenv").config();
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+// app.use(
+//   cors({
+//     origin: "*",
+//   })
+// );
+
+app.get("/", (request, response) => {
+  response.json({ info: "Node.js, Express, and Postgres API" });
+  // response.sendFile(__dirname + "/index.html");
+});
+app.get("/foods", db.getFoods);
+app.post("/insert-food", db.insertFood);
+app.post("/populateFoods", db.populateFoods);
+// app.get("/foodItem", db.foodItem);
+// app.get("/foodCategory", db.foodCategory);
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});
