6b023a4d72bc0966291bd01ad976ade92f7c9d6d
Scwhwennesen1 <eaa23besc@students.eaaa.dk>
2024-05-22 08:45:21 +0200

    kode

--- cirkel_d3.js
+++ cirkel_d3.js
@@ -4,21 +4,21 @@ let mainData;
 let shoppingBasketData = [];
 
 const rectData = [
-  { width: 120, height: 90, category: "Grøntsager og grøntsagsprodukter" },
-  { width: 90, height: 70, category: "Kød og fjerkræ" },
-  { width: 38, height: 38, category: "Vin. øl og spiritus" },
-  { width: 45, height: 45, category: "Brød og bageartikler" },
-  { width: 55, height: 55, category: "Korn og kornprodukter" },
-  { width: 65, height: 65, category: "Færdigretter" },
-  { width: 20, height: 20, category: "Svampe og svampeprodukter" },
-  { width: 20, height: 20, category: "Nødder og frø" },
-  { width: 25, height: 25, category: "Slik og sukkervarer" },
-  { width: 70, height: 70, category: "Frugt og frugtprodukter" },
-  { width: 55, height: 55, category: "Planteprodukter og -drikke" },
-  { width: 80, height: 80, category: "Fisk og skaldyr" },
-  { width: 53, height: 53, category: "Smagsgivere og krydderier" },
-  { width: 50, height: 50, category: "Mælk. mejeriprodukter og æg" },
-  { width: 30, height: 30, category: "Bælgfrugter og bælgfrugtprodukter" },
+  { width: 40, height: 40, category: "Grøntsager og grøntsagsprodukter" },
+  { width: 40, height: 40, category: "Kød og fjerkræ" },
+  { width: 40, height: 40, category: "Vin. øl og spiritus" },
+  { width: 40, height: 40, category: "Brød og bageartikler" },
+  { width: 40, height: 40, category: "Korn og kornprodukter" },
+  { width: 40, height: 40, category: "Færdigretter" },
+  { width: 40, height: 40, category: "Svampe og svampeprodukter" },
+  { width: 40, height: 40, category: "Nødder og frø" },
+  { width: 40, height: 40, category: "Slik og sukkervarer" },
+  { width: 40, height: 40, category: "Frugt og frugtprodukter" },
+  { width: 40, height: 40, category: "Planteprodukter og -drikke" },
+  { width: 40, height: 40, category: "Fisk og skaldyr" },
+  { width: 40, height: 40, category: "Smagsgivere og krydderier" },
+  { width: 40, height: 40, category: "Mælk. mejeriprodukter og æg" },
+  { width: 40, height: 40, category: "Bælgfrugter og bælgfrugtprodukter" },
   { width: 40, height: 40, category: "Drikkevarer" },
 ];
 
@@ -52,7 +52,10 @@ svg
   .enter()
   .append("text")
   .attr("x", (d, i) => (i % cols) * (200 + spacing) + 100 + d.width / 2)
-  .attr("y", (d, i) => Math.floor(i / cols) * (150 + spacing) + 50 + d.height / 2)
+  .attr(
+    "y",
+    (d, i) => Math.floor(i / cols) * (150 + spacing) + 50 + d.height / 2
+  )
   .attr("text-anchor", "middle")
   .attr("font-size", "16px")
   .attr("font-family", "Arial, sans-serif")
@@ -85,41 +88,81 @@ function handleCategoryClick(category) {
     });
 }
 
-// Function to show popup
 function showPopup(category, foodItems) {
-  const sanitizedCategory = sanitizeCategoryName(category);
-  const popup = d3.select("body")
+  // We sort alphabetically
+  foodItems.sort((a, b) => {
+    const productA = a.produkt.toLowerCase();
+    const productB = b.produkt.toLowerCase();
+
+    if (productA < productB) {
+      return -1;
+    }
+    if (productA > productB) {
+      return 1;
+    }
+    return 0;
+  });
+  
+  const popup = d3
+    .select("body")
+
     .append("div")
     .attr("class", "popup")
     .style("position", "fixed")
-    .style("left", "50%")
+    .style("left", "40%")
     .style("top", "50%")
     .style("transform", "translate(-50%, -50%)")
     .style("background", "white")
     .style("padding", "20px")
+    .style("width", "65vw")
     .style("border", "1px solid black")
-    .style("box-shadow", "0 0 10px rgba(0,0,0,0.5)");
+    .style("box-shadow", "0 0 10px rgba(0,0,0,0.5)")
+    .style("display", "flex")
+    .style("flex-direction", "column");
 
   popup.append("h2").text(category);
-  const foodItemContainer = popup.append("div");
+  const foodItemContainer = popup
+    .append("div")
+    .style("display", "flex")
+    .style("flex-wrap", "wrap");
 
-  foodItemContainer.selectAll(".foodItem")
+  foodItemContainer
+    .selectAll(".foodItem")
     .data(foodItems)
     .enter()
     .append("div")
     .attr("class", "foodItem")
     .style("margin", "5px 0")
+    .style("width", "12%")
+    .style("padding", "10px")
+    .style("border-top", "2px solid black")
     .style("cursor", "pointer")
-    .text(d => d.produkt)
-    .on("click", function(event, d) {
+    .text((d) => d.produkt)
+    .on("click", function (event, d) {
       addToShoppingBasket(d);
+    })
+
+    .on("mouseover", function () {
+      d3.select(this)
+        .style("background-color", "lightgrey")
+        .style("color", "red") // Change text color to black on hover
+        .style("border-radius", "10px");
+    })
+    .on("mouseout", function () {
+      d3.select(this)
+        .style("background-color", "white") // Revert to original background color
+        .style("color", "black") // Revert to original text color
+        .style("border-radius", "0"); // Revert to original border radius
     });
 
-  popup.append("button")
-    .text("Close")
-    .on("click", function() {
+  // Close popup when clicking outside
+  d3.select(document).on("click.popup", function (event) {
+    const isClickedOutside = !popup.node().contains(event.target);
+    if (isClickedOutside) {
       popup.remove();
-    });
+      d3.select(document).on("click.popup", null); // Remove the click event listener
+    }
+  });
 }
 
 // Function to add item to the shopping basket
@@ -150,11 +193,11 @@ function emptyBasket() {
   shoppingBasketData = [];
 }
 
-
 // Function to remove food item rectangles and text
 function removeFoodItemRects(category) {
   const sanitizedCategory = sanitizeCategoryName(category);
-  svg.selectAll(`.foodItemGroup-${sanitizedCategory}`)
+  svg
+    .selectAll(`.foodItemGroup-${sanitizedCategory}`)
     .transition()
     .duration(500)
     .attr("opacity", 0)
